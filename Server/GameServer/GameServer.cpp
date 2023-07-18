3ae86f79d2ad152ad16ccc2ff9e0b25a090ac415
jjeonghoon <kjh981001@naver.com>
2023-07-18 23:20:01 +0900

    atomic
    멀티쓰레드 동기화

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,10 +1,43 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+
+// atomic : All or Nothing
+atomic<int> sum = 0;
+
+void Add()
+{
+	for (int i = 0; i < 100'0000; ++i)
+	{
+		sum.fetch_add(1);
+		// sum++; // 이것도 문제 없음.
+	}
+}
+
+void Sub()
+{
+	for (int i = 0; i < 100'0000; ++i)
+	{
+		sum.fetch_add(-1);
+		// sum--;
+	}
+}
+
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-    HelloWorld();
+	Add();
+	Sub();
+	cout << sum << endl;
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+	cout << sum << endl;
 }
 
