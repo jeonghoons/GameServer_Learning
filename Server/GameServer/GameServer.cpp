1cd88fa25aa674fe0d62bf571a2bec6a333fc912
jjeonghoon <kjh981001@naver.com>
2023-07-19 04:33:35 +0900

    lock

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -3,41 +3,65 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-// atomic : All or Nothing
-atomic<int> sum = 0;
+vector<int32> v;
 
-void Add()
+// Mutual Exclusive(상호배타적)
+mutex m;
+
+// RAII 
+template<typename T>
+class LockGuard
 {
-	for (int i = 0; i < 100'0000; ++i)
+private:
+	T* _mutex;
+
+public:
+	LockGuard(T& m)
 	{
-		sum.fetch_add(1);
-		// sum++; // 이것도 문제 없음.
+		_mutex = &m;
+		_mutex->lock();
+	}
+	~LockGuard()
+	{
+		_mutex->unlock();
+	}
+
+};
+
+void Push()
+{
+	for (int32 i = 0; i < 1'0000; ++i)
+	{
+		// m.lock();
+
+		// LockGuard<std::mutex> lockGuard(m); // 자동으로 소멸하기 위해 락가드
+		std::lock_guard<std::mutex> lockGuard(m); // C++에서 지원해주는거
+
+		// std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock); // 당장 락을 잠구진 않고 원하는 시점에서 lock가능
+		// uniqueLock.lock();
+
+		v.push_back(i);
+
+		if (i == 5000) {
+			// m.unlock();
+			break;
+		}
+
+		// m.unlock();
 	}
 }
-
-void Sub()
-{
-	for (int i = 0; i < 100'0000; ++i)
-	{
-		sum.fetch_add(-1);
-		// sum--;
-	}
-}
-
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << endl;
-
-	std::thread t1(Add);
-	std::thread t2(Sub);
+	v.reserve(2'0000);
+	std::thread t1(Push);
+	std::thread t2(Push);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
+	cout << v.size() << endl;
 }
 
