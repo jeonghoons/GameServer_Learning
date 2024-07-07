f7aceadca3197508b4bcbc9363c171e8bcb88ea5
jeonghoons <116326836+jeonghoons@users.noreply.github.com>
2024-07-08 00:59:40 +0900

    Socket Util 라이브러리 추가

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,122 +7,19 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include <winsock2.h>
-#include <mswsock.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
+#include "SocketUtils.h"
 
-void HandleError(const char* cause)
-{
-	int32 errCode = ::WSAGetLastError();
-	cout << cause << " ErrorCode : " << errCode << endl;
-}
-
-const int32 BUFSIZE = 1000;
-
-struct Session
-{
-	WSAOVERLAPPED overlapped = {}; // Session의 주소와 overlapped구조체의 주소가 동일
-	SOCKET socket = INVALID_SOCKET;
-	char recvBuffer[BUFSIZE] = {};
-	int32 recvBytes = 0;
-};
-
-void CALLBACK RecvCallback(DWORD error, DWORD recvLen, LPWSAOVERLAPPED overlapped, DWORD flags)
-{
-	cout << "Data Recv Len Callback = " << recvLen << endl;
-
-	Session* session = (Session*)overlapped;
-}
 
 int main()
 {
-	WSAData wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		return 0;
+	SOCKET socket = SocketUtils::CreateSocket();
 
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
-		return 0;
+	SocketUtils::BindAnyAddress(socket, 7777);
+	SocketUtils::Listen(socket);
 
-	u_long on = 1;
-	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
-		return 0;
+	SOCKET clientSocket = ::accept(socket, nullptr, nullptr);
 
-	SOCKADDR_IN serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
+	cout << "Client Connected " << endl;
 
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-		return 0;
-
-	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-		return 0;
-
-	cout << "Accept" << endl;
-
-	
-
-
-	while (true)
-	{
-		SOCKADDR_IN clientAddr;
-		int32 addrLen = sizeof(clientAddr);
-
-		SOCKET clientSocket;
-		while (true)
-		{
-			clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-			if (clientSocket != INVALID_SOCKET)
-				break;
-
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
-
-			// 문제 있는 상황
-			return 0;
-		}
-
-		Session session = Session{ clientSocket };
-		
-
-		cout << "Client Connected !" << endl;
-
-		while (true)
-		{
-			WSABUF wsaBuf;
-			wsaBuf.buf = session.recvBuffer;
-			wsaBuf.len = BUFSIZE;
-
-			DWORD recvLen = 0;
-			DWORD flags = 0;
-			if (::WSARecv(clientSocket, &wsaBuf, 1, &recvLen, &flags, &session.overlapped, RecvCallback) == SOCKET_ERROR)// 비동기 Recv
-			{
-				if (::WSAGetLastError() == WSA_IO_PENDING)
-				{
-					// Pending
-					// Alertable Wait
-
-					::SleepEx(INFINITE, TRUE);
-				}
-				else
-				{
-					// TODO : 문제 있는 상황
-					break;
-				}
-			}
-			else {
-				cout << "Data Recv Len = " << recvLen << endl;
-			}
-		}
-
-		::closesocket(session.socket);
-		
-	}
-
-
-	// 윈속 종료
-	::WSACleanup();
+	while (true) {}
 }
